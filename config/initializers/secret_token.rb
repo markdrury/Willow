f3f47e07d5de1508d4e311ac0cac6968d4f81ac9
Mark Drury <mark@drury.com>
2011-09-29 16:09:27 -0700

    Adding standard rails files to the repo.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Willow::Application.config.secret_token = 'db8112d7cd3a5fbecf6770913a0df013ba0b623f61ab5e1cc3000b738354d8a258ac6b74041a6f11c63893dc8b0d4f1555e0e8744b24dc35b1c5dd4219ab8840'
